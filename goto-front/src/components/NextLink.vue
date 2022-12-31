ec3ec9b5e376a1bfe4e223aea30214c92b318abf
liangmi <10700132+liangmiqwq@user.noreply.gitee.com>
2022-12-31 03:42:08 +0000

    有的地方还是要用土办法的改一改的啦
    Signed-off-by: liangmi <10700132+liangmiqwq@user.noreply.gitee.com>

---
+++ goto-front/src/components/NextLink.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="nextLink">
+    </div>
+</template>
+<script>
+    export default {
+        name: "nextLink",
+    }
+</script>
+<style>
+    @keyframes goto {
+        from {
+            margin-right: 100%;
+        }
+
+        to {
+            margin-right: 0px;
+        }
+    }
+
+    #textOfGoto {
+        position: absolute;
+    }
+
+    #nextLink {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: calc(100% - 68px - 73.5px);
+        background-color: rgb(204, 237, 245);
+        animation-name: goto;
+        animation-duration: 3s;
+    }
+</style>
---
+++ goto-front/src/components/TextOfMain.vue
@@ -0,0 +1,25 @@
+<template>
+    <h1 id="TextOfMain">正在GoTo到下一个Link...</h1>
+
+</template>
+<script>
+    export default {
+        name: "TextOfMain"
+    }
+</script>
+<style>
+    #TextOfMain {
+        display: block;
+        position: absolute;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        margin: auto;
+        width: 400px;
+        text-align: center;
+        height: 5em;
+        line-height: 2em;
+        /* margin-bottom: 88px; */
+    }
+</style>
